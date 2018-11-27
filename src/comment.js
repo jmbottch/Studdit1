887b604789a6b38a24a25a71afeb7d75de91f414
Joep <jgj.gruter@student.avans.nl>
2018-11-27 01:28:50 +0100

    Added models and test helper file

---
+++ src/comment.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+    content: String,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    comments: [{
+        type: Schema.Types.ObjectId,
+        ref: 'comment'
+    }]
+});
+
+const Comment = mongoose.model('comment', CommentSchema);
+
+module.exports = Comment;
---
+++ test/test_helper.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/studdit');
+
+    mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => { console.warn('Error: ', error);});
+});
+
+beforeEach((done) => {
+    const { users, comments, threads } = mongoose.connection.collections;
+    users.drop(() => {
+        comments.drop(() => {
+            threads.drop(() => {
+                done();
+            });
+        });
+    });
+});
