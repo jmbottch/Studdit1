cd24865e33f15a3d746dc7629bb7b43553a996d6
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-11-27 17:05:28 +0100

    nog een keer dan

--- server.js
+++ server.js
@@ -1,52 +1,49 @@
 const MongoClient = require('mongodb').MongoClient
 var ObjectID = require('mongodb').ObjectID;
 const express = require('express');
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const app = express();
 const User = require('./src/models/user');
 
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.favicon());
+app.use(express.static(settings.c.WEB_PATH + '/public'));
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.methodOverride());
+app.use(useragent.express());
 
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.configure(function(){
-    app.use(express.favicon());
-    app.use(express.static(settings.c.WEB_PATH + '/public'));
-    app.use(express.bodyParser());
-    app.use(express.cookieParser());
-    app.use(express.methodOverride());
-    app.use(useragent.express());
-  
-    app.use(passport.initialize());
-    app.use(passport.session());
-});
 
 MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', (err, db) => {
-  var dbase = db.db("studditmongo");
-  if (err) return console.log(err)
-  app.listen(3000, () => {
-    console.log('app working on 3000')
-  })
+    var dbase = db.db("studditmongo");
+    if (err) return console.log(err)
+    app.listen(3000, () => {
+        console.log('app working on 3000')
+    })
 
-  app.get('/api', (req, res) => {
-    res.send({test: 'succes'});
-});
-
-app.post('user/add', (req, res, next) => {
-    var user = new User({
-      username: req.body.username,
-      password: req.body.password,
-      threads: [req.body.threads]
+    app.get('/api', (req, res) => {
+        res.send({ test: 'succes' });
     });
 
-    dbase.collection("user").save(user, (err, result) => {
-      if(err) {
-        console.log(err);
-        
-      }
+    app.post('user/add', (req, res, next) => {
+        var user = new User({
+            username: req.body.username,
+            password: req.body.password,
+            threads: [req.body.threads]
+        });
 
-      res.send('User added successfully');
+        dbase.collection("user").save(user, (err, result) => {
+            if (err) {
+                console.log(err);
+
+            }
+
+            res.send('User added successfully');
+        });
     });
-  });
 });
