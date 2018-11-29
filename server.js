1195430c3849ab96e97d4e81609ffaa98c19e4dc
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-11-29 17:54:21 +0100

    yeet

--- server.js
+++ server.js
@@ -5,18 +5,31 @@ const bodyParser = require('body-parser')
 const routes = require('./routes/routes');
 const app = express();
 const User = require('./src/models/user');
+const neo4j = require('neo4j-driver').v1;
+
+
+
+
+
+const driver = neo4j.driver('bolt://hobby-ccflbaaccbcogbkemkneffbl.dbs.graphenedb.com:24786', 
+neo4j.auth.basic('jeroen', 'b.m1mQSF3xsOsB.5LbY3TnSpyUejZ6B'), console.log("Neo4j is connected on port 3000"));
+
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', (err, db) => {
-  var dbase = db.db("studditmongo");
+MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', {useNewUrlParser: true}, (err, db) => {
+ 
+var dbase = db.db("studditmongo");
   if (err) return console.log(err)
-  app.listen(process.env.PORT || 3000, () => {
-    console.log('app working on 3000')
+  app.listen(process.env.PORT || 3050, () => {
+    console.log('MongoDB is connected on 3050')
   })
 
   routes(app);
 });
+
