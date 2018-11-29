d7c710e4d4b1d74a088192632ede203acefe7e20
Joep <jgj.gruter@student.avans.nl>
2018-11-29 19:07:10 +0100

    Fixed connection ports

--- server.js
+++ server.js
@@ -6,28 +6,17 @@ const routes = require('./routes/routes');
 const app = express();
 const User = require('./src/models/user');
 const neo4j = require('neo4j-driver').v1;
-
-
-
-
+var mongodb = require('./src/config/mongodb_connector');
+const config = require('./src/config/mongodb_config');
 
 const driver = neo4j.driver('bolt://hobby-ccflbaaccbcogbkemkneffbl.dbs.graphenedb.com:24786', 
 neo4j.auth.basic('jeroen', 'b.m1mQSF3xsOsB.5LbY3TnSpyUejZ6B'), console.log("Neo4j is connected on port 3000"));
 
-
-
-
 app.use(bodyParser.json());
 routes(app);
 
-MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', {useNewUrlParser: true}, (err, db) => {
- 
-var dbase = db.db("studditmongo");
-  if (err) return console.log(err)
-  app.listen(process.env.PORT || 3050, () => {
-    console.log('MongoDB is connected on 3050')
-  })
+app.listen(config.env.webPort, () => {
+  console.log('App is ready for requests on localhost:3050')
+})
 
-  routes(app);
-});
-
+module.exports = app;
