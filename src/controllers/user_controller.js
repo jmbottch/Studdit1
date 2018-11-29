98cb4648bb2fdbe50625f7aa45ccfbf35687af91
Joep <jgj.gruter@student.avans.nl>
2018-11-29 13:45:07 +0100

    Updated project structure

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -1,27 +1,56 @@
 const User = require('../models/user');
+const MongoClient = require('mongodb').MongoClient
 
 module.exports = {
 
-    createUser(req, res, next) {
-        const userProps = req.body;
+    test(req, res) {
+        res.send({
+            test: 'succes'
+        });
+        console.log('succes bitch');
+    },
 
-        User.create(userProps)
-            .then(user => res.send(user))
-            .catch(next);
+    createUser(req, res) {
+        const userProps = req.body;
+        const user = new User(userProps);
+
+        MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', (err, db) => {
+            var dbase = db.db("studditmongo");
+            if (err) return console.log(err)
+
+            // Checks if username is already taken
+            if (dbase.collection("user").countDocuments({
+                    username: user.username
+                })
+                .then((count) => {
+                    // If the username is already taken
+                    if (count > 0) {
+                        res.send('Username already taken');
+                    }
+                    // If the username is not already taken
+                    else {
+                        // Save the user to the database
+                        dbase.collection("user").insertOne(user, (err, result) => {
+                            if (err) {
+                                console.log(err);
+                            }
+                            res.send('User added successfully');
+                        });
+                    }
+                }));
+        });
     },
 
     editUser(req, res, next) {
         const userId = req.params.id;
         const userProps = req.body;
 
-        User.findByIdAndUpdate({
-                _id: userId
-            }, userProps)
-            .then(() => User.findById({
-                _id: userId
-            }))
-            .then(driver => res.send(driver))
-            .catch(next);
+        MongoClient.connect('mongodb://admin:admin123@ds151631.mlab.com:51631/studditmongo', (err, db) => {
+            var dbase = db.db("studditmongo");
+            if (err) return console.log(err)
+
+            dbase.collection("user").findByIdAndUpdate({ _id: userId }, userProps);
+        });
     },
 
     deleteUser(req, res, next) {
