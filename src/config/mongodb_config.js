27d76c6c4a7c36a7154048e88f28735dc116ce52
Joep Gruter <jgj.gruter@student.avans.nl>
2019-04-08 09:50:10 +0200

    Updated mlab credentials

--- src/config/mongodb_config.js
+++ src/config/mongodb_config.js
@@ -1,9 +1,9 @@
 var env = {
-    webPort: process.env.PORT || 3000,
+    webPort: process.env.PORT || 3050,
     dbHost: process.env.DB_HOST || 'ds151631.mlab.com',
     dbPort: process.env.DB_PORT || '51631',
-    dbUser: process.env.DB_USER || 'jmbottch',
-    dbPassword: process.env.DB_PASSWORD || 'admin',
+    dbUser: process.env.DB_USER || 'dbUser',
+    dbPassword: process.env.DB_PASSWORD || 'dbPassword1',
     dbDatabase: process.env.DB_DATABASE || 'studditmongo'
 }
 
@@ -11,9 +11,7 @@ var env = {
 //     'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
 //     'mongodb://localhost/' + env.dbDatabase
 
-var dburl = process.env.NODE_ENV === 'production' ?
-    'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase :
-    'mongodb://localhost/' + env.dbDatabase
+var dburl = 'mongodb://' + env.dbUser + ':' + env.dbPassword + '@' + env.dbHost + ':' + env.dbPort + '/' + env.dbDatabase;
 
 module.exports = {
     env: env,
